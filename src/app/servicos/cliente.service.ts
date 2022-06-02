279b905a158556ca7cb8d2f6dc2be127a7fc39df
Douglas Ribeiro <douglas_jipa@outlook.com>
2022-06-01 21:56:34 -0300

    arquivo cliente.service.ts - pg 24

--- src/app/servicos/cliente.service.ts
+++ src/app/servicos/cliente.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+export interface Cliente{
+  id: number;
+  nome: string;
+  telefone: string;
+  endereco: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private url = 'http://localhost/apiAppCrud/apiCliente';
+
+  constructor(private htpp: HttpClient) { }
 }
